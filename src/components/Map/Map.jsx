26ea8c265a82fa5a84025282e480bdc279fda3ed
Alin Lechintan <alin.lechintan@outlook.com>
2025-01-04 16:50:18 +0200

    update new Map component
    out with google maps api, in with leaflet

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,45 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 import styles from "./Map.module.css";
 
+const customIcon = new L.Icon({
+  iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
 function Map() {
-  return <div></div>;
+  const position = [45.92425, 23.48361];
+  return (
+    <section className={styles.mapSection}>
+      <h2 className={styles.subtitle}>Locația Noastră</h2>
+      <div className={styles.mapWrapper}>
+        <MapContainer
+          center={position}
+          zoom={13}
+          style={{ height: "500px", width: "100%" }}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={position} icon={customIcon}>
+            <Popup>
+              Conacul Dante
+              <p>Strada Horea, nr. 117c</p>
+              <p>517536</p>
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+    </section>
+  );
 }
 
 export default Map;
