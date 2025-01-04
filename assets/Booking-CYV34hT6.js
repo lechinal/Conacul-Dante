e9629b7756d1f76928d56178d1b7b6d725290c31
Alin Lechintan <alin.lechintan@outlook.com>
2025-01-04 17:05:01 +0200

    Updates

--- assets/Booking-CYV34hT6.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-LKS6-0w2.js";function n(){return o.jsx("div",{children:"Booking"})}export{n as default};
---
+++ assets/Contact-GmrgmLDu.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CwFagoza.js";function r(){return t.jsx("div",{children:t.jsx("p",{children:"Contact"})})}export{r as default};
