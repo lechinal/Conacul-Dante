525e1852a6782106deee5a132b7e3e44577c92a0
Alin Lechintan <alin.lechintan@outlook.com>
2025-01-07 10:37:42 +0200

    update Header-Navigation
    updated Header-Navigation on mobile and tablet with ::after pseudo element

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -40,7 +40,10 @@ function Map() {
           <MapContainer
             center={position}
             zoom={13}
-            style={{ height: "500px", width: "100%" }}
+            style={{
+              height: "500px",
+              width: "100%",
+            }}
           >
             <TileLayer
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
